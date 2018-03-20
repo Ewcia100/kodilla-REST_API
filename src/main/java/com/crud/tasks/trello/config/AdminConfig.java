185befe3a4f81a4b13f94b8ee2d71857cd549544
Ewcia100 <epawelska@gmail.com>
2018-03-20 14:10:53 +0100

    Tworzenie szablonu i wysyłanie maili

--- src/main/java/com/crud/tasks/trello/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/trello/config/AdminConfig.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Component;
 @Component
 @Getter
 public class AdminConfig {
+    @Value("${admin.name}")
+    private String adminName;
    @Value("${admin.mail}")
     private String adminMail;
 }

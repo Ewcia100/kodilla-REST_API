9dfa8166bf9f04f6541c24e655eba6a67c459ed8
Ewcia100 <epawelska@gmail.com>
2019-02-21 23:23:22 +0100

    test part three

--- src/main/java/com/crud/tasks/domain/Tests.java
+++ src/main/java/com/crud/tasks/domain/Tests.java
@@ -2,4 +2,20 @@ package com.crud.tasks.domain;
 public class Tests {
     private Long id;
     private String message;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }

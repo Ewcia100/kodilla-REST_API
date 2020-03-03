580b538d83092da804023533552860a3b1245747
eweli <epawelska@gmail.com>
2020-03-03 22:27:56 +0100

    FIRST

---
+++ src/main/java/com/crud/tasks/domain/Task2.java
@@ -0,0 +1,11 @@
+package com.crud.tasks.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Task2 {
+    private Task task;
+    private String sth;
+}
---
+++ src/main/java/com/crud/tasks/domain/Task3.java
@@ -0,0 +1,5 @@
+package com.crud.tasks.domain;
+
+public class Task3 {
+    private String sth3;
+}
